76ef6c8387e103421e39806ea73cbbacd533ae14
Simone Veitch <simone.veitch@gmail.com>
2024-03-04 13:45:59 +1100

    Completed lab

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+/* My first try which works in replit
+function add(total, value) {
+    return total + value;
+}
+
+let totalBatteries = batteryBatches.reduce(add);*/
+
+const totalBatteries = batteryBatches.reduce((total, current) => total + current, 0 );
+
